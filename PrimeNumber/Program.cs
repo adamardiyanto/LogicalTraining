55d122ac9f95265ed7331264b7e48c82cac10532
Adam <adamardiyantow@gmail.com>
2023-11-17 11:19:27 +0700

    Prime Number Training

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("enter number = ");
+        int number;
+        int.TryParse(Console.ReadLine(),out number);
+        string result = CheckPrime(number) ? "Number is Prime" : "Number is not Prime";
+        Console.WriteLine(result);
+    }
+
+    public static bool CheckPrime(int number)
+    {
+        bool isPrime = true;
+
+        for(int i = 2; i <= number /2; i++)
+        {
+            if(number % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+
+        return isPrime;
+    }
+}
