1fda5e35926400359571164f464ed9716838af75
Adam <adamardiyantow@gmail.com>
2023-11-17 14:16:59 +0700

    Reverse string

---
+++ ReverseString/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("enter text = ");
+        string text = Console.ReadLine();
+        string reverse = string.Empty;
+        for(int i=text.Length-1; i >= 0; i--)
+        {
+            reverse += text[i];
+        }
+        System.Console.WriteLine(reverse);
+    }
+}
