999006ab99f23fba3d6c8304f889762cd5ca2af5
Adam <adamardiyantow@gmail.com>
2023-11-17 13:12:30 +0700

    palindrome training

---
+++ Palindrome/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("enter text = ");
+        string text = Console.ReadLine();
+        string result = IsPalindrome(text) ? text + " is Palindrome" : text + " is not Palindrome";
+        Console.WriteLine(result);
+    }
+
+    public static bool IsPalindrome(string text)
+    {
+        bool result = true;
+        int length = text.Length;
+        for(int i = 0; i< length/2; i++)
+        {
+            if(text[i] != text[length-1-i])
+            {
+                result = false;
+                return result;
+            }
+        } 
+        return result;
+    }
+}
