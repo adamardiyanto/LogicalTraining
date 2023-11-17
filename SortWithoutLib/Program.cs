d3e09e4bcfa40e004b934f65abfa834c0d2b31e6
Adam <adamardiyantow@gmail.com>
2023-11-17 16:50:50 +0700

    sorting number

---
+++ SortWithoutLib/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        int[] array = new int[] {3,4,2,7,5,1,6,8};
+        foreach(var x in array)
+        {
+            System.Console.Write(x);
+        }
+        int temp = 0;
+        for(int i = 0; i <= array.Length; i++)
+        {
+            for(int j = i+1; j<array.Length; j++)
+            {
+                if (array[i]< array[j])
+                {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        Console.ReadLine();
+        foreach(var x in array)
+        {
+            System.Console.Write(x);
+        }
+    }
+}
