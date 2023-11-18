64f1a2accc4bf64915b3ad325eaea0798f78bf6f
Adam <adamardiyantow@gmail.com>
2023-11-18 19:22:33 +0700

    remove duplicate string

---
+++ DuplicateString/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Progam
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("enter text = ");
+        string input = Console.ReadLine();
+        string result = string.Empty;
+        for(int i=0; i< input.Length; i++)
+        {
+            if(!result.Contains(input[i]))
+            {
+                result += input[i];
+            }
+        }
+
+        System.Console.WriteLine(result);
+
+    }
+}
